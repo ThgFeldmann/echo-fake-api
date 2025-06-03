84d96f96b369043512bdaa7501d31418ee410ca3
Thiago <thiagofeldmann06@gmail.com>
2025-06-03 16:17:41 -0300

    fix: renamed main file and removed json-server-auth
    may revert these changes on the future

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const jsonServer = require('json-server');
-const auth = require('json-server-auth');
 const server = jsonServer.create();
-const router = jsonServer.router('api.json');
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-server.db = router.db
-
 server.use(middlewares);
-server.use(auth);
 server.use(router);
-server.listen(3000, () => {
+server.listen(5000, () => {
     console.log('JSON Server is running!')
 })
